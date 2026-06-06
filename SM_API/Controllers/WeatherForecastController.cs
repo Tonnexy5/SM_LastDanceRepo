8ac1f09f52e3add86b77a1daac5c0aac678ad7e4
Anthony Pennant Allen <90796529+Tonnexy5@users.noreply.github.com>
2026-06-05 19:43:44 -0600

    Commit inicial

---
+++ SM_API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SM_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+   
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SM_API/SM_API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace SM_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+   
+        [HttpGet(Name = "ObtenerDatos")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
