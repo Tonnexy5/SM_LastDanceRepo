33e9cbbdfd1556f92b4f1fbe6d70a7f59297df2c
Anthony Pennant Allen <90796529+Tonnexy5@users.noreply.github.com>
2026-06-26 23:07:39 -0600

    Validacion en Front end

---
+++ SM_Web/wwwroot/js/index.js
@@ -0,0 +1,40 @@
+﻿$(function () {
+
+    $("#IndexForm").validate({
+        rules: {
+            correoElectronico: {
+                required: true,
+                email: true
+            },
+            Contrasenna: {
+                required: true,
+                minlength: 5
+            }
+        },
+        messages: {
+            correoElectronico: {
+                required: "Campo obligatorio",
+                email: "Formato no válido"
+            },
+            Contrasenna: {
+                required: "Campo obligatorio",
+                minlength: "Mínimo 5 caracteres"
+            }
+        },
+        errorElement: "span",
+        errorPlacement: function (error, element) {
+            error.addClass("text-danger small d-block");
+            element.closest(".form-group").after(error);
+        },
+        highlight: function (element) {
+            $(element).addClass("is-invalid");
+        },
+        unhighlight: function (element) {
+            $(element).removeClass("is-invalid").addClass("is-valid");
+        },
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+
+});
---
+++ SM_Web/wwwroot/js/registro.js
@@ -0,0 +1,58 @@
+﻿$(function () {
+
+    $.validator.addMethod("caracterEspecial", function (value, element) {
+        return this.optional(element) || /[!@#$%^&*(),.?":{}|<>]/.test(value);
+    }, "");
+
+    $("#RegistroForm").validate({
+        rules: {
+            identificacion: {
+                required: true
+            },
+            nombre: {
+                required: true
+            },
+            correoElectronico: {
+                required: true,
+                email: true
+            },
+            Contrasenna: {
+                required: true,
+                minlength: 5,
+                caracterEspecial: true
+            }
+        },
+        messages: {
+            identificacion: {
+                required: "Campo obligatorio"
+            },
+            nombre: {
+                required: "Campo obligatorio"
+            },
+            correoElectronico: {
+                required: "Campo obligatorio",
+                email: "Formato no válido"
+            },
+            Contrasenna: {
+                required: "Campo obligatorio",
+                minlength: "Mínimo 6 caracteres",
+                caracterEspecial: "Al menos 1 caracter especial"
+            }
+        },
+        errorElement: "span",
+        errorPlacement: function (error, element) {
+            error.addClass("text-danger small d-block");
+            element.closest(".form-group").after(error);
+        },
+        highlight: function (element) {
+            $(element).addClass("is-invalid");
+        },
+        unhighlight: function (element) {
+            $(element).removeClass("is-invalid").addClass("is-valid");
+        },
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+
+});
